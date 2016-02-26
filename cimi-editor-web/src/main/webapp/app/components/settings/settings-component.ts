93fed647157754ba5235504ed7b68927c3feb12d
Patrick Langford <patrick@neuronsong.com>
2016-02-25 18:23:04 -0700

    added settings component for controlling editor settings

---
+++ cimi-editor-web/src/main/webapp/app/components/settings/settings-component.ts
@@ -0,0 +1,67 @@
+import {Component} from "angular2/core";
+import {FormBuilder, FORM_DIRECTIVES} from "angular2/common";
+
+@Component({
+    selector: "settings",
+    directives: [FORM_DIRECTIVES],
+    templateUrl: "./app/components/settings/settings.html",
+    providers: [FormBuilder]
+})
+
+export class SettingsComponent {
+    //constructor() { };
+
+    private payload = null;
+
+    private model-patterns = ["observation"];
+
+    private model-meta-data = {
+    'observation':
+        {
+            'model-name':'observation',
+            'model':'CIMI-CORE-ITEM_GROUP.observation_result.v1.0.0.json',
+            'elements-to-hide':
+                {
+                    'Subject of information': true,
+                    'Cluster': true,
+                    'Element': true,
+                    'item': true
+                }
+        }
+    };
+/*
+    'procedure':
+        {
+            'model-name':'???',
+            'model':'CIMI-CORE-ITEM_GROUP.???.v1.0.0.json',
+            'elements-to-hide':
+                {
+                    'Subject of information': true,
+                    'Cluster': true,
+                    'Element': true,
+                    'item': true
+                }
+        },
+    'assertion':
+        {
+            'model-name':'???',
+            'model':'CIMI-CORE-ITEM_GROUP.???.v1.0.0.json',
+            'elements-to-hide':
+                {
+                    'Subject of information': true,
+                    'Cluster': true,
+                    'Element': true,
+                    'item': true
+                }
+        }
+    };
+*/
+    private submitted: boolean = false;
+
+    public onSubmit() {
+        this.submitted = true;
+        // TODO: Store decision somewhere
+        this.payload = JSON.stringify(this.model.pattern);
+        alert(this.payload);
+    }
+}
