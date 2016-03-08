6a27711915b50baac402bf0f648a297b1860252e
Patrick Langford <patrick@neuronsong.com>
2016-03-08 13:57:56 -0700

    added model viewer component

---
+++ cimi-editor-web/src/main/webapp/app/components/model-viewer/model-viewer.service.ts
@@ -0,0 +1,21 @@
+// Description: Provides API service layer for the Model Viewer feature
+
+import {Injectable} from "angular2/core";
+import {Http, Response} from "angular2/http";
+// Import any local models
+import {Observable} from "rxjs/Observable";
+// TODO: Create service core for constants
+// import {ServiceCore} from "../../shared/ServiceCore";
+
+
+@Injectable()
+export class ModelViewerService {
+    constructor (private http: Http) {}
+
+    private handleError (error: Response) {
+    // TODO: Log error somewhere more permanent
+    // TODO: Differentiate between different errors
+    console.error(error);
+    return Observable.throw(error.json().error || "Server error");
+    }
+}
