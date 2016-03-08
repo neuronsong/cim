4f488f4e655aa7eb376e4fd3fe0caf0d046e92cb
Patrick Langford <patrick@neuronsong.com>
2016-03-08 13:43:12 -0700

    minor fixes to model creator

--- cimi-editor-web/src/main/webapp/app/components/model-creator/model-creator.service.ts
+++ cimi-editor-web/src/main/webapp/app/components/model-creator/model-creator.service.ts
@@ -1,38 +1,26 @@
-// Description: Provides API service layer for the Pattern Selector feature
+// Description: Provides API service layer for the Model Creator feature
 
 import {Injectable} from "angular2/core";
 import {Http, Response} from "angular2/http";
 // Import any local models
-// import {Pattern} from "../../model/pattern";
 import {Observable} from "rxjs/Observable";
 // TODO: Create service core for constants
 // import {ServiceCore} from "../../shared/ServiceCore";
 
 
 @Injectable()
-export class PatternSelectorService {
+export class ModelCreatorService {
     constructor (private http: Http) {}
 
-    // TODO: Get from ServiceCore
-    private _serviceUrl = "http://www.google.com";  // URL to web api
-
-    getPatterns() {
-        return [];
-
-        // TODO: Enable once services are up
-        // return this.http.get(this._serviceUrl)
-        //     .map(res => <Pattern[]> res.json().data)
-        //     .catch(this.handleError);
-    }
-
-    // Expects stringified JSON object containing the selected pattern
-    storePattern (pattern: string) {
+    // Expects stringified JSON object containing the new model
+    storeModel (pattern: string) {
         // TODO: Link to services
         // return this.http.post(this._serviceUrl, pattern)
         //     .map(res => <Pattern[]> res.json().data)
         //     .catch(this.handleError);
-        localStorage.setItem("cimiPattern", pattern);
+        localStorage.setItem("cimiModel", pattern);
     }
+    
     private handleError (error: Response) {
     // TODO: Log error somewhere more permanent
     // TODO: Differentiate between different errors
